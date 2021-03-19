c403007d251a5f281b49aea514891b705ae49cdf
Vanessa720 <vanessawilliamson718@gmail.com>
2021-03-19 18:48:17 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Write your solution in this file!
+let employee = {
+    name : 'arnold'}
+
+function updateEmployeeWithKeyAndValue(employee,key,value){
+  delete employee.key;
+  let copyemployee= {... employee};
+  copyemployee[key] = value ;
+  console.log(copyemployee);
+  console.log(employee.value);
+  return copyemployee;
+  }
+
+function destructivelyUpdateEmployeeWithKeyAndValue (employee,key,value) {
+  employee[key] = value ;
+  console.log(employee);
+  return employee ;
+  }
+
+
+function deleteFromEmployeeByKey(employee,key){
+  let cemployee= { ...employee};
+  delete cemployee[key];
+  return cemployee;
+
+  }
+
+function destructivelyDeleteFromEmployeeByKey(employee,key){
+  let coemployee= { ...employee};
+  console.log(employee);
+  delete employee[key];
+  delete coemployee[key];
+  console.log(coemployee);
+  return employee;
+  }
